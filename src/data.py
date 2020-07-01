5dc3e5d4585c9428f21643c55744fba3bc4ecfaa
spartazhihu <wanglun@berkeley.edu>
2020-07-01 13:24:02 -0700

    add data

--- src/data.py
+++ src/data.py
@@ -2,15 +2,39 @@ import sys
 sys.path.append('../infimnist_py')
 import _infimnist as infimnist
 import numpy as np
+from torch.utils.data import Dataset, DataLoader
 
-mnist = infimnist.InfimnistGenerator()
-indexes = np.array([0, 10000, 70000], dtype=np.int64)
-digits, labels = mnist.gen(indexes)
+FEATURE_TEMPLATE = '../data/infimnist_feature_%d_%d.npy'
+TARGET_TEMPLATE = '../data/infimnist_target_%d_%d.npy'
 
-# example of preprocessing from [0, 255] to [0., 1.]
-X = digits.astype(np.float32).reshape(indexes.shape[0], 28, 28)
-X = X / 255
+# should I include the target in the sample?
+class MyDataset(Dataset):
+    def __init__(self, feature_path, target_path, transform=None):
+        self.feature = np.load(feature_path)
+        self.target = np.load(target_path)
+        self.transform = transform
+    
+    def __getitem__(self, idx):
+        sample = self.feature[idx]
+        if self.transform:
+            sample = self.transform(sample)
+        return sample, self.target[idx]
+    
+    def __len__(self):
+        return self.target.shape[0]
 
-import matplotlib.pyplot as plt
-plt.imshow(X[0])
-plt.title('label: {}'.format(labels[0]))
+def gen_infimnist(start=0, end=10000):
+    mnist = infimnist.InfimnistGenerator()
+    indexes = np.array(np.arange(start, end), dtype=np.int64)
+    digits, labels = mnist.gen(indexes)
+    digits = digits.astype(np.float32).reshape(-1, 28, 28)
+    # print(digits.shape)
+    np.save(FEATURE_TEMPLATE%(start, end), digits)
+    np.save(TARGET_TEMPLATE%(start, end), labels)
+
+if __name__ == '__main__':
+    gen_infimnist(0, 100)
+    dataset_loader = DataLoader(MyDataset(FEATURE_TEMPLATE%(0,100), TARGET_TEMPLATE%(0,100)))
+    examples = enumerate(dataset_loader)
+    batch_idx, (feature, target) = next(examples)
+    print(batch_idx, feature, target)
--- src/simulate.py
+++ src/simulate.py
@@ -1,2 +1,51 @@
+import argparse
 import numpy as np
-from networks import 
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from networks import MultiLayerPerceptron
+from data import gen_infimnist, MyDataset
+
+FEATURE_TEMPLATE = '../data/infimnist_feature_%d_%d.npy'
+TARGET_TEMPLATE = '../data/infimnist_target_%d_%d.npy'
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', default='0')
+    parser.add_argument('--dataset', default='INFIMNIST')
+    parser.add_argument('--nworker', type=int)
+    parser.add_argument('--perround', type=int)
+    parser.add_argument('--localiter', type=int)
+    parser.add_argument('--up_bit')
+    parser.add_argument('--down_bit')
+    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--momentum')
+    parser.add_argument('--weightdecay')
+    parser.add_argument('--network')
+    args = parser.parse_args()
+
+    DEVICE = "cuda:" + args.device
+    DATASET = args.dataset
+    NWORKER = args.nworker
+    PERROUND = args.perround
+    LOCALITER = args.localiter
+    LEARNING_RATE = args.lr
+
+    if DATASET == 'INFIMNIST':
+
+        transform=torchvision.transforms.Compose([
+                                       torchvision.transforms.ToTensor(),
+                                       torchvision.transforms.Normalize(
+                                         (0.1307,), (0.3081,))])
+        batch_size = 5
+        dataset_loader = DataLoader(MyDataset(FEATURE_TEMPLATE%(0,100), TARGET_TEMPLATE%(0,100), transform=transform), batch_size=batch_size, shuffle=True)
+
+        network = MultiLayerPerceptron()
+        #TODO: do we need momentum?
+        optimizer = optim.SGD(network.parameters(), lr=LEARNING_RATE)
+        
+        examples = enumerate(dataset_loader)
+        batch_idx, (feature, target) = next(examples)
+        print(feature)
+
+        
