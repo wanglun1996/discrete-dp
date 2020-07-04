79f1b19557b78dcf2e28174d3034a426deccce7a
Lun Wang wanglun@berkeley.edu <lunwang@pinwheel5.d2.comp.nus.edu.sg>
2020-07-04 10:59:54 +0800

    finish mnist test

--- src/data.py
+++ src/data.py
@@ -2,10 +2,11 @@ import sys
 sys.path.append('../infimnist_py')
 import _infimnist as infimnist
 import numpy as np
+import argparse
 from torch.utils.data import Dataset, DataLoader
 
-FEATURE_TEMPLATE = '../data/infimnist_feature_%d_%d.npy'
-TARGET_TEMPLATE = '../data/infimnist_target_%d_%d.npy'
+FEATURE_TEMPLATE = '../data/infimnist_%s_feature_%d_%d.npy'
+TARGET_TEMPLATE = '../data/infimnist_%s_target_%d_%d.npy'
 
 # should I include the target in the sample?
 class MyDataset(Dataset):
@@ -23,18 +24,30 @@ class MyDataset(Dataset):
     def __len__(self):
         return self.target.shape[0]
 
-def gen_infimnist(start=0, end=10000):
+def gen_infimnist(start=0, end=10000, split=0.8):
     mnist = infimnist.InfimnistGenerator()
     indexes = np.array(np.arange(start, end), dtype=np.int64)
     digits, labels = mnist.gen(indexes)
     digits = digits.astype(np.float32).reshape(-1, 28, 28)
+    sidx = int(end * split)
+    train_digits = digits[:sidx]
+    test_digits = digits[sidx:]
+    train_labels = labels[:sidx]
+    test_labels = labels[sidx:]
     # print(digits.shape)
-    np.save(FEATURE_TEMPLATE%(start, end), digits)
-    np.save(TARGET_TEMPLATE%(start, end), labels)
+    np.save(FEATURE_TEMPLATE%('train', start, end), train_digits)
+    np.save(TARGET_TEMPLATE%('train', start, end), train_labels)
+    np.save(FEATURE_TEMPLATE%('test', start, end), test_digits)
+    np.save(TARGET_TEMPLATE%('test', start, end), test_labels)
 
 if __name__ == '__main__':
-    gen_infimnist(0, 100)
-    dataset_loader = DataLoader(MyDataset(FEATURE_TEMPLATE%(0,100), TARGET_TEMPLATE%(0,100)))
-    examples = enumerate(dataset_loader)
-    batch_idx, (feature, target) = next(examples)
-    print(batch_idx, feature, target)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--size', type=int, default=10000)
+    args = parser.parse_args()
+
+    gen_infimnist(0, args.size)
+    # dataset_loader = DataLoader(MyDataset(FEATURE_TEMPLATE%(0,100), TARGET_TEMPLATE%(0,100)))
+    # examples = enumerate(dataset_loader)
+    # batch_idx, (feature, target) = next(examples)
+    # print(batch_idx, feature, target)
