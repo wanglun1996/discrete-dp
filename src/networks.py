6aefeb864c6711d1119e311b925bba3220f90b32
Lun Wang wanglun@berkeley.edu <lunwang@pinwheel5.d2.comp.nus.edu.sg>
2020-07-09 13:10:57 +0800

    add cifar

--- src/networks.py
+++ src/networks.py
@@ -16,6 +16,24 @@ class MultiLayerPerceptron(nn.Module):
         x = F.log_softmax(self.fc3(x))
         return x
 
+class ConvNet(nn.Module):
+
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, stride=1, padding=2)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=2)
+        self.fc1 = nn.Linear(64 * 6 * 6, 384)
+        self.fc2 = nn.Linear(384, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 6 * 6)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
 class BasicBlock(nn.Module):
     expansion = 1
 
--- src/simulate.py
+++ src/simulate.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 import torchvision
 from torch.utils.data import Dataset, DataLoader, random_split
-from networks import MultiLayerPerceptron
+from networks import MultiLayerPerceptron, ConvNet
 from data import gen_infimnist, MyDataset
 import torch.nn.functional as F
 from torch import nn, optim, hub
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     parser.add_argument('--nworker', type=int, default=100)
     parser.add_argument('--perround', type=int, default=10)
     parser.add_argument('--localiter', type=int, default=5)
-    parser.add_argument('--epoch', type=int, default=100) 
+    parser.add_argument('--epoch', type=int, default=200) 
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--up', type=int, default=32)
     parser.add_argument('--momentum')
@@ -50,23 +50,36 @@ if __name__ == '__main__':
 
         # read in the dataset with numpy array split them and then use data loader to wrap them
         train_set = MyDataset(FEATURE_TEMPLATE%('train',0,10000), TARGET_TEMPLATE%('train',0,10000), transform=transform)
-        # Split into multiple training set
-        TRAIN_SIZE = len(train_set) // NWORKER
-        sizes = []
-        sum = 0
-        for i in range(0, NWORKER):
-            sizes.append(TRAIN_SIZE)
-            sum = sum + TRAIN_SIZE
-        sizes[0] = sizes[0] + len(train_set)  - sum
-        train_sets = random_split(train_set, sizes)
-        train_loaders = []
-        for trainset in train_sets:
-            train_loaders.append(DataLoader(trainset, **params))
-
         test_loader = DataLoader(MyDataset(FEATURE_TEMPLATE%('test',0,10000), TARGET_TEMPLATE%('test',0,10000), transform=transform), batch_size=BATCH_SIZE)
 
 
         network = MultiLayerPerceptron().to(device)
+
+    elif DATASET == 'CIFAR10':
+
+        transform = torchvision.transforms.Compose([
+                                         torchvision.transforms.CenterCrop(24), 
+                                         torchvision.transforms.ToTensor(), 
+                                         torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+        train_set = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transform)
+        test_loader = DataLoader(torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform))
+
+        network = ConvNet().to(device)
+        print(network)
+
+    # Split into multiple training set
+    TRAIN_SIZE = len(train_set) // NWORKER
+    sizes = []
+    sum = 0
+    for i in range(0, NWORKER):
+        sizes.append(TRAIN_SIZE)
+        sum = sum + TRAIN_SIZE
+    sizes[0] = sizes[0] + len(train_set)  - sum
+    train_sets = random_split(train_set, sizes)
+    train_loaders = []
+    for trainset in train_sets:
+        train_loaders.append(DataLoader(trainset, **params))
 
     params = list(network.parameters())
     criterion = nn.CrossEntropyLoss()
@@ -115,7 +128,8 @@ if __name__ == '__main__':
 
             for iepoch in range(0, LOCALITER):
                 for idx, (feature, target) in enumerate(train_loaders[c], 0):
-                    feature = torch.flatten(feature, start_dim=1).to(device)
+                    # feature = torch.flatten(feature, start_dim=1).to(device)
+                    feature = feature.to(device)
                     target = target.type(torch.long).to(device)
                     optimizers[c].zero_grad()
                     output = network(feature)
@@ -138,8 +152,21 @@ if __name__ == '__main__':
             for i in range(0, len(global_model)):
                 global_model[i] = global_model[i] + local_models_diff[c][i] / PERROUND
 
+        if epoch % 10 == 0:
+            test_loss = 0
+            correct = 0
+            with torch.no_grad():
+                for feature, target in test_loader:
+                    feature = feature.to(device)
+                    target = target.type(torch.long).to(device)
+                    output = network(feature)
+                    test_loss += F.nll_loss(output, target, size_average=False).item()
+                    pred = output.data.max(1, keepdim=True)[1]
+                    correct += pred.eq(target.data.view_as(pred)).sum()
+            test_loss /= len(test_loader.dataset)
+            print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
+
 """
-        if epoch % 1 == 0:
             correct = 0
             total = 0
             for i, data in enumerate(test_loader, 0):
@@ -164,9 +191,7 @@ if __name__ == '__main__':
 
 
     print('Finished Training')
-"""
 
-"""
         # from here
         for t in range(EPOCH):
             for batch_idx, (feature, target) in enumerate(train_loader):
